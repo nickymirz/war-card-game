a048a39e5d8ddbcf252f1f0b66cbbdd6bccffcda
Nicky Mirz <nickymirz@gmail.com>
2020-07-11 13:20:30 +0300

    Updates

---
+++ precache-manifest.5b7d33a8d49c280b17baecb029bcf795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab48c326ec0d7bab034c84e90d8b1ef8",
+    "url": "/war-card-game/index.html"
+  },
+  {
+    "revision": "d5678941dd1be2077bce",
+    "url": "/war-card-game/static/css/main.df9decc2.chunk.css"
+  },
+  {
+    "revision": "c16eb1d363b4c9bdc6d8",
+    "url": "/war-card-game/static/js/2.3855e76f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/war-card-game/static/js/2.3855e76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5678941dd1be2077bce",
+    "url": "/war-card-game/static/js/main.6fbcba9c.chunk.js"
+  },
+  {
+    "revision": "8e4eb60a65c135e64570",
+    "url": "/war-card-game/static/js/runtime-main.81a3e354.js"
+  }
+]);
